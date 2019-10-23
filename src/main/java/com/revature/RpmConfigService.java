2b1702832b2843307746ff49cac817ebb622619e
Wezley Singleton <wezley.singleton@gmail.com>
2019-10-22 23:14:22 -0400

    changed base class name to RpmConfigService; removed unneeded JwtConfig class

--- src/main/java/com/revature/RpmConfigService.java
+++ src/main/java/com/revature/RpmConfigService.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServiceApplication {
+public class RpmConfigService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
+		SpringApplication.run(RpmConfigService.class, args);
 	}
 }
